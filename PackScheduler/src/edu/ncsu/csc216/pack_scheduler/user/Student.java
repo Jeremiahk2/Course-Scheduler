6bdff6bfade6f0dfa6a5fed6d99db7aa7d595032
Geigh Neill <gdneill@ncsu.edu>
2022-08-31 16:02:22 -0400

    [Implementation] Filling out program shell

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
@@ -1,27 +1,62 @@
 package edu.ncsu.csc216.pack_scheduler.user;
 
+/**
+ * Student class
+ * @author Geigh Neill
+ *
+ */
 public class Student {
 	
+	/** The max credits a student can have */
 	public static final int MAX_CREDITS = 18;
 
+	/**
+	 * Student constructor
+	 * @param firstName student's first name
+	 * @param lastName student's last name
+	 * @param id student's id number
+	 * @param email student's email
+	 * @param hashPW  
+	 * @param maxCredits maximum credits student can have
+	 */
 	public Student(String firstName, String lastName, String id, String email, String hashPW, int maxCredits) {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Student constructor
+	 * @param firstName student's first name
+	 * @param lastName student's last name
+	 * @param id student's id number
+	 * @param email student's email
+	 * @param hashPW
+	 */
 	public Student(String firstName, String lastName, String id, String email, String hashPW) {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * Returns first name of student
+	 * @return student's first name
+	 */
 	public String getFirstName() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Returns last name of student
+	 * @return student's last name
+	 */
 	public String getLastName() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	
+	/**
+	 * Returns id of student
+	 * @return student's id
+	 */
 	public String getId() {
 		// TODO Auto-generated method stub
 		return null;
