cd92c4add9435c163e8881511b93052c5e37e43d
Sahil Kanchan <skancha6@ncsu.edu>
2022-09-10 18:09:14 -0400

    [Implementation] Worked on readStudentRecords() and writeStudentRecords()

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIO.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIO.java
@@ -1,20 +1,64 @@
 package edu.ncsu.csc216.pack_scheduler.io;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import edu.ncsu.csc216.pack_scheduler.user.Student;
+import edu.ncsu.csc216.wolf_scheduler.course.Course;
 
+/**
+ * 
+ * @author Sahil Kanchan
+ *
+ */
 public class StudentRecordIO {
 
+	/**
+	 * 
+	 * @param fileName
+	 * @return
+	 * @throws FileNotFoundException
+	 */
 	public static ArrayList<Student> readStudentRecords(String fileName) throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		Scanner fileReader = new Scanner(new FileInputStream(fileName));
+		ArrayList<Student> newStudent = new ArrayList<Student>();
+		while (fileReader.hasNextLine()) {
+			try {
+				Student student = processStudent(fileReader.nextLine());
+				newStudent.add(student);
+			}
+			catch (IllegalArgumentException e){
+				
+			}
+		}
+		fileReader.close();
+		return newStudent;
+		
 	}
-
+	
+	/**
+	 * 
+	 * @param fileName
+	 * @param studentDirectory
+	 * @throws IOException
+	 */
 	public static void writeStudentRecords(String fileName, ArrayList<Student> studentDirectory) throws IOException {
-		// TODO Auto-generated method stub
+		PrintStream fileWriter = new PrintStream(new File(fileName));
+		
+		for (int i = 0; i < studentDirectory.size(); i++) {
+    	    fileWriter.println(studentDirectory.get(i).toString());
+    	}
+
+    	fileWriter.close();
+		
+	}
+	
+	private static Student processStudent(String line) {
 		
 	}
 
