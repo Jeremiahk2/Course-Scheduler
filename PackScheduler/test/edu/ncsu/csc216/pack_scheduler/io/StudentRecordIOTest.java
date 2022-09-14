875ea998ff6d6d318d25dcb65d460d2bcd3a777e
jdknizle <jdknizle@ncsu.edu>
2022-09-14 16:11:26 -0400

    [Skeleton] Created StudentRecordIOTest skeleton

---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.pack_scheduler.io;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Jerry
+ *
+ */
+class StudentRecordIOTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.io.StudentRecordIO#readStudentRecords(java.lang.String)}.
+	 */
+	@Test
+	void testReadStudentRecords() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.io.StudentRecordIO#writeStudentRecords(java.lang.String, java.util.ArrayList)}.
+	 */
+	@Test
+	void testWriteStudentRecords() {
+		fail("Not yet implemented");
+	}
+
+}
