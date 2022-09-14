0f5d96b160dd7411b3b5cfcb50988cfd57aca9d0
Sahil Kanchan <skancha6@ncsu.edu>
2022-09-14 15:49:41 -0400

    [Implementation] Created StudentTest.java

---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/StudentTest.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.pack_scheduler.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Sahil Kanchan
+ *
+ */
+class StudentTest {
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.user.Student#hashCode()}.
+	 */
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.user.Student#Student(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, int)}.
+	 */
+	@Test
+	void testStudentStringStringStringStringStringInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.user.Student#Student(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	void testStudentStringStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.user.Student#setEmail(java.lang.String)}.
+	 */
+	@Test
+	void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.user.Student#setPassword(java.lang.String)}.
+	 */
+	@Test
+	void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.user.Student#setMaxCredits(int)}.
+	 */
+	@Test
+	void testSetMaxCredits() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.user.Student#setFirstName(java.lang.String)}.
+	 */
+	@Test
+	void testSetFirstName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.user.Student#setLastName(java.lang.String)}.
+	 */
+	@Test
+	void testSetLastName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.user.Student#equals(java.lang.Object)}.
+	 */
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.pack_scheduler.user.Student#toString()}.
+	 */
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
