cbc9de95061fdb3b80b54cbfb2c864a46d06253f
Jeremiah Knizley <jdknizle@ncsu.edu>
2022-10-02 00:17:45 -0400

    [Implementation] Finished CourseCatalog.java Wrote tests for same class

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalog.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalog.java
@@ -6,6 +6,7 @@ package edu.ncsu.csc216.pack_scheduler.catalog;
 import edu.ncsu.csc217.collections.list.SortedList;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import edu.ncsu.csc216.pack_scheduler.course.Course;
 import edu.ncsu.csc216.pack_scheduler.io.CourseRecordIO;
@@ -35,9 +36,106 @@ public class CourseCatalog {
 	 */
 	public void loadCoursesFromFile(String fileName) {
 		try {
-			catalog = CourseRecordIO.readCourseRecords(fileName); //TODO: refactor CourseRecordIO to only use SortedLists
+			catalog = CourseRecordIO.readCourseRecords(fileName);
 		} catch (FileNotFoundException e) {
 			throw new IllegalArgumentException("Cannot find file.");
+		}
+	}
+	
+	/**
+	 * adds a course to the course catalog
+	 * @param name the name of the course
+	 * @param title the title of the course
+	 * @param section the section of the course
+	 * @param credits the number of credits the course gives
+	 * @param instructorId the ID of the instructor teaching the course
+	 * @param meetingDays the days the course meets for class (if applicable)
+	 * @param startTime start time of class meetings in military time
+	 * @param endTime the end time of class meetings in military time
+	 * @return boolean true if successfully added, false if not.
+	 */
+	public boolean addCourseToCatalog(String name, String title, String section, int credits, String instructorId, String meetingDays, int startTime, int endTime) throws IllegalArgumentException {
+		Course course = new Course(name, title, section, credits, instructorId, meetingDays, startTime, endTime);
+		for (int i = 0; i < catalog.size(); i++) {
+			Course catalogCourse = catalog.get(i);
+			String courseName = catalogCourse.getName();
+			String courseSection = catalogCourse.getSection();
+			if (courseName.equals(name) && courseSection.equals(section)) {
+				return false;
+			}
+		}
+		catalog.add(course);
+		return true;
+	}
+	/**
+	 * Removes a course from the catalog
+	 * @param name the name of the course to be removed
+	 * @param section the section of the course to be removed
+	 * @return boolean true if successfully removed, false if there was nothing to remove
+	 */
+	public boolean removeCourseFromCatalog(String name, String section) {
+		int index = -1;
+		for (int i = 0; i < catalog.size(); i++) {
+			Course catalogCourse = catalog.get(i);
+			String courseName = catalogCourse.getName();
+			String courseSection = catalogCourse.getSection();
+			if (courseName.equals(name) && courseSection.equals(section)) {
+				index = i;
+			}
+		}
+		if (index != -1) {
+			catalog.remove(index);
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * Returns the specified course from the catalog
+	 * @param name the name of the course to be returned
+	 * @param section the section of the course to be returned
+	 * @return the course requested, or null if the course is not in the catalog
+	 */
+	public Course getCourseFromCatalog(String name, String section) {
+		int index = -1;
+		for (int i = 0; i < catalog.size(); i++) {
+			Course catalogCourse = catalog.get(i);
+			String courseName = catalogCourse.getName();
+			String courseSection = catalogCourse.getSection();
+			if (courseName.equals(name) && courseSection.equals(section)) {
+				index = i;
+			}
+		}
+		if (index != -1) {
+			return catalog.get(index);
+		}
+		return null;
+	}
+	/**
+	 * Returns a 2D array containing information for all courses in the catalog.
+	 * Information in the array is (for each course): name, section, title, meeting info.
+	 * @return a 2d array containing information for all courses in the catalog
+	 */
+	public String[][] getCourseCatalog() {
+		if (catalog.isEmpty()) {
+			return new String[0][0];
+		}
+		String[][] courseCatalog = new String[catalog.size()][3]; 
+		for (int i = 0; i < catalog.size(); i++) {
+			Course currentCourse = catalog.get(i);
+			courseCatalog[i] = currentCourse.getShortDisplayArray();
+		}
+		
+		return courseCatalog;
+	}
+	/**
+	 * Writes the full catalog of courses in toString format to a text file with the given file name
+	 * @param fileName the name of the file to write to
+	 */
+	public void saveCourseCatalog(String fileName) {
+		try {
+			CourseRecordIO.writeCourseRecords(fileName, catalog);
+		} catch (IOException e) {
+			throw new IllegalArgumentException("The file cannot be saved");
 		}
 	}
 	
--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalogTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalogTest.java
@@ -1,5 +1,150 @@
 package edu.ncsu.csc216.pack_scheduler.catalog;
 
-public class CourseCatalogTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ncsu.csc216.pack_scheduler.course.Course;
+
+/**
+ * Tests CourseCatalog implementation into PackScheduler
+ * @author Jeremiah Knizley
+ *
+ */
+public class CourseCatalogTest {
+	/** Valid course records */
+	private final String validTestFile = "test-files/course_records.txt";
+	/** Invalid course records */
+	private final String invalidTestFile = "test-files/invalid_course_records.txt";
+	
+	/** Course name */
+	private static final String NAME = "CSC 216";
+	/** Course title */
+	private static final String TITLE = "Software Development Fundamentals";
+	/** Course section */
+	private static final String SECTION = "001";
+	/** Course credits */
+	private static final int CREDITS = 3;
+	/** Course instructor id */
+	private static final String INSTRUCTOR_ID = "sesmith5";
+	/** Course meeting days */
+	private static final String MEETING_DAYS = "TH";
+	/** Course start time */
+	private static final int START_TIME = 1330;
+	/** Course end time */
+	private static final int END_TIME = 1445;
+	
+	/** Event title */
+	private static final String EVENT_TITLE = "Exercise";
+	/** Event meeting days */
+	private static final String EVENT_MEETING_DAYS = "MTWHF";
+	/** Event start time */
+	private static final int EVENT_START_TIME = 800;
+	/** Event end time */
+	private static final int EVENT_END_TIME = 900;
+	/** Event details */
+	private static final String EVENT_DETAILS = "Cardio Time!";
+	/**
+	 * Resets course_records.txt for use in other tests.
+	 */
+	@Before
+	public void setUp() throws Exception {
+		//Reset course_records.txt so that it's fine for other needed tests
+		Path sourcePath = FileSystems.getDefault().getPath("test-files", "starter_course_records.txt");
+		Path destinationPath = FileSystems.getDefault().getPath("test-files", "course_records.txt");
+		try {
+			Files.deleteIfExists(destinationPath);
+			Files.copy(sourcePath, destinationPath);
+		} catch (IOException e) {
+			fail("Unable to reset files");
+		}
+	}
+	/**
+	 * Test CourseCatalog.saveCourseCatalog
+	 */
+	@Test
+	public void testSaveCourseCatalog() {
+		//Test that empty catalog saves successfully
+		CourseCatalog cc = new CourseCatalog();
+		cc.saveCourseCatalog("test-files/actual_empty_export.txt");
+		checkFiles("test-files/expected_empty_export.txt", "test-files/actual_empty_export.txt");
+		
+		//Add courses and test that exports correctly
+		cc.addCourseToCatalog("CSC 116", "Intro to Programming - Java", "003", 3, "spbalik", "MW", 1250, 1440);
+		cc.addCourseToCatalog("CSC 216", "Software Development Fundamentals", "001", 3, "sesmith5", "MW", 1330, 1445);
+		cc.addCourseToCatalog("CSC 216", "Software Development Fundamentals", "601", 3, "jctetter", "A", 0, 0);
+		assertEquals(3, cc.getCourseCatalog().length);
+		cc.saveCourseCatalog("test-files/actual_course_records.txt");
+		checkFiles("test-files/expected_course_records.txt", "test-files/actual_course_records.txt");
+	}
+	
+	private void checkFiles(String expFile, String actFile) {
+		try (Scanner expScanner = new Scanner(new File(expFile));
+			 Scanner actScanner = new Scanner(new File(actFile));) {
+			String test = "-1";
+			while (actScanner.hasNextLine()) {
+				test = expScanner.nextLine();
+				assertEquals(test, actScanner.nextLine());
+				
+			}
+			if (expScanner.hasNextLine()) {
+				fail();
+			}
+			
+			expScanner.close();
+			actScanner.close();
+		} catch (IOException e) {
+			fail("Error reading files.");
+		}
+	}
+	/**
+	 * Tests CourseCatalog.addCourseToCatalog, newCourseCatalog, and removeCourseFromCatalog
+	 */
+	@Test
+	public void testAddCourseToCatalog() {
+		CourseCatalog catalog = new CourseCatalog();
+		
+		catalog.addCourseToCatalog("CSC 116", "Intro to Programming - Java", "003", 3, "spbalik", "MW", 1250, 1440);
+		catalog.addCourseToCatalog("CSC 216", "Software Development Fundamentals", "001", 3, "sesmith5", "MW", 1330, 1445);
+		catalog.addCourseToCatalog("CSC 216", "Software Development Fundamentals", "601", 3, "jctetter", "A", 0, 0);
+		assertEquals(3, catalog.getCourseCatalog().length);
+		
+		assertTrue(catalog.removeCourseFromCatalog("CSC 116", "003"));
+		assertEquals(2, catalog.getCourseCatalog().length);
+		assertFalse(catalog.removeCourseFromCatalog("CSC 116", "003"));
+		assertEquals(2, catalog.getCourseCatalog().length);
+		
+		catalog.newCourseCatalog();
+		assertEquals(0, catalog.getCourseCatalog().length);
+	}
+	
+	/**
+	 * Tests loadCoursesFromFile for CourseCatalog
+	 */
+	@Test
+	public void testLoadCoursesFromFile() {
+		CourseCatalog catalog = new CourseCatalog();
+		assertEquals(0, catalog.getCourseCatalog().length);
+		catalog.loadCoursesFromFile("test-files/expected_course_records.txt");
+		assertEquals(3, catalog.getCourseCatalog().length);
+		Course course1 = new Course("CSC 116", "Intro to Programming - Java", "003", 3, "spbalik", "MW", 1250, 1440);
+		Course course2 = new Course("CSC 216", "Software Development Fundamentals", "001", 3, "sesmith5", "MW", 1330, 1445);
+		Course course3 = new Course("CSC 216", "Software Development Fundamentals", "601", 3, "jctetter", "A", 0, 0);
+		assertTrue(course1.equals(catalog.getCourseFromCatalog("CSC 116", "003")));
+		assertTrue(course2.equals(catalog.getCourseFromCatalog("CSC 216", "001")));
+		assertTrue(course3.equals(catalog.getCourseFromCatalog("CSC 216", "601")));
+		
+		
+	}
 }
