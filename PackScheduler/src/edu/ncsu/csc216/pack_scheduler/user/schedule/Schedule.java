5e8a8b12fe43ac97ff6227568ef1358bdb9577ee
Daniel Grattan <dsgratta@ncsu.edu>
2022-10-30 18:59:03 -0400

    Written ScheduleTest.java

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/schedule/Schedule.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/schedule/Schedule.java
@@ -3,10 +3,84 @@
  */
 package edu.ncsu.csc216.pack_scheduler.user.schedule;
 
+import edu.ncsu.csc216.pack_scheduler.course.Course;
+import edu.ncsu.csc216.pack_scheduler.util.ArrayList;
+
 /**
+ * Class for a Schedule object. 
  * @author Jeremiah Knizley
- *
+ * @author Spencer Grattan
  */
 public class Schedule {
-
+	
+	/** ArrayList to contain all of the courses in the schedule */
+	private ArrayList<Course> schedule;
+	/** The title of the schedule */
+	private String title;
+	
+	/**
+	 * Constructor for Schedule
+	 */
+	public Schedule() {
+		this.setTitle("My Schedule");
+		this.schedule = new ArrayList<Course>();
+	}
+	
+	/**
+	 * Adds a course to the end of the schedule
+	 * @param course the course being added to the schedule
+	 * @return true if the course was able to be added 
+	 * @throws IllegalArgumentException if there is a duplicate course
+	 * or if there is a course conflict with an existing course
+	 * @throws NullPointerException if the course added is null
+	 */
+	public boolean addCourseToSchedule(Course course) {
+		
+	}
+	
+	/**
+	 * Removes a course from the schedule 
+	 * @param course the course to be removed
+	 * @return true if the course was successfully removed
+	 * false if there was not a course to remove
+	 */
+	public boolean removeCourseFromSchedule(Course course) {
+		
+	}
+	
+	/**
+	 * Creates a new schedule ArrayList and resets the title to default
+	 */
+	public void resetSchedule() {
+		this.setTitle("My Schedule");
+		this.schedule = new ArrayList<Course>();
+	}
+	
+	/**
+	 * Gets a 2d array of course information where each row is a course
+	 * and each column is name, section, title, meetingString
+	 * @return all course information
+	 */
+	public String[][] getScheduledCourses() {
+		
+	}
+	
+	/**
+	 * Sets the title of the schedule
+	 * @param title the title to be set
+	 */
+	public void setTitle(String title) {
+		if (title == null) {
+			throw new IllegalArgumentException("Title cannot be null.");
+		}
+		this.title = title;
+	}
+	
+	/**
+	 * Gets the title of the schedule
+	 * @return the title
+	 */
+	public String getTitle() {
+		return this.title;
+	}
 }
--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/schedule/ScheduleTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/schedule/ScheduleTest.java
@@ -8,22 +8,135 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import edu.ncsu.csc216.pack_scheduler.course.Course;
+
 /**
+ * Test the Schedule Class 
+ * @author Spencer Grattan
  * @author Jeremiah Knizley
  *
  */
 class ScheduleTest {
- 
+	
 	/**
-	 * @throws java.lang.Exception
+	 * Tests the Schedule constructor
 	 */
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSchedule() {
+		//tests for default constructor values
+		Schedule s1 = assertDoesNotThrow(() -> new Schedule());
+		assertEquals("My Schedule", s1.getTitle());
+		String[][] emptyTestArray = new String[][] {};
+		assertArrayEquals(emptyTestArray, s1.getScheduledCourses());
 	}
+	
+	/**
+	 * Tests the addCourseToSchedule method
+	 */
+	@Test
+	void testAddCourseToSchedule() {
+		Schedule s1 = new Schedule();
+		Course c = new Course("CSC123", "Learning to type", "001", 3, "pbnutter", "MWF", 1300, 1400);
+		
+		//tests for valid course addition
+		assertDoesNotThrow(() -> s1.addCourseToSchedule(c));
+		String[][] testArray = new String[][] { {"CSC123", "001", "Learning to type", c.getMeetingString()} };
+ 		assertArrayEquals(testArray, s1.getScheduledCourses());
+		
+		//tests for adding duplicate course
+		Course c2 = new Course("CSC123", "Learning to type", "001", 3, "pbnutter", "MWF", 1500, 1600);
+		Exception e1 = assertThrows(IllegalArgumentException.class, 
+				() -> s1.addCourseToSchedule(c2));
+		assertEquals("You are already enrolled in CSC123.", e1.getMessage());
+		
+		//tests for adding conflicting course
+		Course c3 = new Course("CSC567", "Intro to Mainframe Hacking", "002", 3, "goobo", "MWF", 1300, 1400);
+		Exception e2 = assertThrows(IllegalArgumentException.class, 
+				() -> s1.addCourseToSchedule(c3));
+		assertEquals("The course cannot be added due to a conflict.", e2.getMessage());
+		
+		//tests for adding null
+		assertThrows(NullPointerException.class, () -> s1.addCourseToSchedule(null));
+	}
+	
+	/**
+	 * Test the removeCourseFromSchedule method
+	 */
+	@Test
+	void testRemoveCourseFromSchedule() {
+		Schedule s1 = new Schedule();
+		Course c = new Course("CSC123", "Learning to type", "001", 3, "pbnutter", "MWF", 1300, 1400);
+		
+		//populate schedule
+		assertDoesNotThrow(() -> s1.addCourseToSchedule(c));
+		
+		//test course not in schedule
+		Course c3 = new Course("CSC567", "Intro to Mainframe Hacking", "002", 3, "goobo", "MWF", 1300, 1400);
+		assertFalse(s1.removeCourseFromSchedule(c3));
+		
+		//test course in schedule
+		assertTrue(s1.removeCourseFromSchedule(c));
+		String[][] testArray = new String[][] {};
+		assertArrayEquals(testArray, s1.getScheduledCourses());
+	}
+	
+	/**
+	 * Tests the resetSchedule method
+	 */
+	@Test
+	void testResetSchedule() {
+		Schedule s1 = new Schedule();
+		Course c = new Course("CSC123", "Learning to type", "001", 3, "pbnutter", "MWF", 1300, 1400);
+		
+		//populate schedule and change title
+		s1.addCourseToSchedule(c);
+		s1.setTitle("mmmmmmmmm");
+		
+		//test reset
+		s1.resetSchedule();
+		assertEquals("My Schedule", s1.getTitle());
+		String[][] testArray = new String[][] {};
+		assertArrayEquals(testArray, s1.getScheduledCourses());
+	}
+	
+	/**
+	 * Tests the getScheduledCorses method
+	 */
+	@Test
+	void testGetScheduledCourses() {
+		//test empty schedule
+		Schedule s1 = new Schedule();
+		String[][] testArray = new String[][] {};
+		assertArrayEquals(testArray, s1.getScheduledCourses());
+		
+		//populate schedule
+		Course c = new Course("CSC123", "Learning to type", "001", 3, "pbnutter", "MWF", 1300, 1400);
+		Course c1 = new Course ("CSC234", "Intro to houses", "001", 3, "scooby", "TH", 1000, 1100);
+		s1.addCourseToSchedule(c);
+		s1.addCourseToSchedule(c1);
+		
+		//test populated schedule
+		String[][] testArray2 = new String[][] {{c.getName(), c.getSection(), c.getTitle(), c.getMeetingString()},
+			{c1.getName(), c1.getSection(), c1.getTitle(), c1.getMeetingString()}};
+		assertArrayEquals(testArray2, s1.getScheduledCourses());
+	}
+	
+	/**
+	 * Tests the setTitle method
+	 */
+	@Test
+	void testSetTitle() {
+		Schedule s1 = new Schedule();
+		//tests valid case
+		s1.setTitle("Wa wa");
+		assertEquals("Wa wa", s1.getTitle());
+		
+		//tests null
+		Exception e1 = assertThrows(IllegalArgumentException.class, 
+				() -> s1.setTitle(null));
+		assertEquals("Title cannot be null.", e1.getMessage());
+	}
+	
+	
 
 }
